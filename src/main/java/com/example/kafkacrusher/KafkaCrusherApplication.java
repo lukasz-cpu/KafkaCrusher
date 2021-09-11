061ab40f4227830e6701de8ba26eaa4c090f3696
lukasz_p <merc.angel@hotmail.com>
2021-09-11 08:29:34 +0200

    initial commit

---
+++ src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkacrusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCrusherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCrusherApplication.class, args);
+    }
+
+}
