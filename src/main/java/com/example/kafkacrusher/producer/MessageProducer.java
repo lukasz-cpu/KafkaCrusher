9b48ccf066ee444607f7ac8d51e97ac226ac5313
lukasz_p <merc.angel@hotmail.com>
2021-09-11 22:10:09 +0200

    asd

--- src/main/java/com/example/kafkacrusher/producer/MessageProducer.java
+++ src/main/java/com/example/kafkacrusher/producer/MessageProducer.java
@@ -1,9 +1,19 @@
 package com.example.kafkacrusher.producer;
 
 import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.common.KafkaFuture;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 @Component
 @AllArgsConstructor
@@ -14,6 +24,27 @@ public class MessageProducer {
 
     @GetMapping("test")
     public void createTopic(){
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.74:9092");
+        AdminClient adminClient = AdminClient.create(config);
+        ListTopicsResult listTopicsResult = adminClient.listTopics();
+        KafkaFuture<Set<String>> names = listTopicsResult.names();
+
+
+
+        try {
+            Set<String> strings = names.get();
+            for (String string : strings) {
+                System.out.println(string);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
         System.out.println("haha");
     }
 }
