ace94ea23a8831197e6f61a6ca8065ac49374f48
lukasz_p <merc.angel@hotmail.com>
2021-09-11 21:02:23 +0200

    zxc

---
+++ src/main/java/com/example/kafkacrusher/model/Connection.java
@@ -0,0 +1,8 @@
+package com.example.kafkacrusher.model;
+
+
+import lombok.Data;
+
+@Data
+public class Connection {
+}
--- src/main/java/com/example/kafkacrusher/producer/MessageProducer.java
+++ src/main/java/com/example/kafkacrusher/producer/MessageProducer.java
@@ -1,18 +1,19 @@
 package com.example.kafkacrusher.producer;
 
 import lombok.AllArgsConstructor;
-import org.apache.kafka.clients.KafkaClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Component
 @AllArgsConstructor
+@RestController
 public class MessageProducer {
 
 
 
     @GetMapping("test")
-    public void sendMessage(){
-
+    public void createTopic(){
+        System.out.println("haha");
     }
 }
