4564811a9b4d795db54fad1850c8ddf0e630a848
lukasz_p <merc.angel@hotmail.com>
2021-09-11 09:02:04 +0200

    asd

---
+++ src/main/java/com/example/kafkacrusher/producer/MessageProducer.java
@@ -0,0 +1,19 @@
+package com.example.kafkacrusher.producer;
+
+import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.KafkaClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Component
+@AllArgsConstructor
+public class MessageProducer {
+
+    KafkaTem
+
+
+    @GetMapping("test")
+    public void sendMessage(){
+
+    }
+}
