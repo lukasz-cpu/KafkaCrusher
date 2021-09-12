bbf1d5e061849cb38be5d1d5d466da759c4eeb42
lukasz_p <merc.angel@hotmail.com>
2021-09-12 18:49:41 +0200

    asd

--- src/main/java/com/example/kafkacrusher/client/connection/ClientConnectionController.java
+++ src/main/java/com/example/kafkacrusher/client/connection/ClientConnectionController.java
@@ -1,6 +1,9 @@
 package com.example.kafkacrusher.client.connection;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +23,16 @@ public class ClientConnectionController {
         return new ResponseEntity("Connection added: " + clientConnectionRequestDTO, HttpStatus.OK);
     }
 
+    @SneakyThrows
     @GetMapping(value = "/getConnections")
     public ResponseEntity<ClientConnectionResponseDTO> getConnections(){
         List<ClientConnectionResponseDTO> connectionsInfo = registrationConnectionService.getConnectionsInfo();
-        return new ResponseEntity(connectionsInfo.toString(), HttpStatus.OK);
+        String payload = getJson(connectionsInfo);
+        return new ResponseEntity(payload, HttpStatus.OK);
     }
 
+    private String getJson(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+     }
 }
