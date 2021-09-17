c359319cfd3158667813fe6768a53e4718b4892c
lukasz_p <merc.angel@hotmail.com>
2021-09-17 09:14:32 +0200

    a

--- src/main/java/com/example/kafkacrusher/client/connection/ConnectionActiveManager.java
+++ src/main/java/com/example/kafkacrusher/client/connection/ConnectionActiveManager.java
@@ -2,24 +2,15 @@ package com.example.kafkacrusher.client.connection;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.DescribeClusterOptions;
-import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Component
 @AllArgsConstructor
