4750cbfbfd9dcae978aa3d764db93f866ffdf838
lukasz_p <merc.angel@hotmail.com>
2021-09-18 15:22:54 +0200

    xzc

--- src/main/java/com/example/kafkacrusher/topic/TopicController.java
+++ src/main/java/com/example/kafkacrusher/topic/TopicController.java
@@ -1,7 +1,5 @@
 package com.example.kafkacrusher.topic;
 
-import org.apache.kafka.clients.KafkaClient;
-import org.apache.kafka.clients.admin.KafkaAdminClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
