ae17e0c0e061b5d1cd406a8d3f07f816847811c2
lukasz_p <merc.angel@hotmail.com>
2021-09-29 09:43:04 +0200

    123

--- src/main/java/com/example/kafkacrusher/messages/MessageService.java
+++ src/main/java/com/example/kafkacrusher/messages/MessageService.java
@@ -1,21 +1,45 @@
 package com.example.kafkacrusher.messages;
 
 
+import com.example.kafkacrusher.connection.ClientConnectionRepository;
+import com.example.kafkacrusher.topic.BrokerNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import static com.example.kafkacrusher.util.JSONUtils.getJson;
 
 @Component
 @Slf4j
 public class MessageService {
 
+    private final ClientConnectionRepository clientConnectionRepository;
+
+    public MessageService(ClientConnectionRepository clientConnectionRepository) {
+        this.clientConnectionRepository = clientConnectionRepository;
+    }
+
     public String processMessageForConnection(MessageDTO message) {
 
         String connectionName = message.getConnectionName();
+        try {
+            String brokerAddressesByName = getBrokerAddressesByName(connectionName);
+        } catch (BrokerNotFoundException e) {
+            e.printStackTrace();
+        }
 
-    
 
         return "";
+    }
+
+
+
+
+
+
+    private String getBrokerAddressesByName(String name) throws BrokerNotFoundException {
+        return clientConnectionRepository.
+                findByConnectionName(name)
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new BrokerNotFoundException("Broker not found"))
+                .getBrokers();
     }
 }
