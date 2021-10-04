8569488f4e9135dfa5ab4522e7fe9697f00784c8
lukasz_p <merc.angel@hotmail.com>
2021-10-04 21:08:29 +0200

    asdasdasd

--- src/main/java/com/example/kafkacrusher/messages/MessageService.java
+++ src/main/java/com/example/kafkacrusher/messages/MessageService.java
@@ -33,13 +33,13 @@ public class MessageService {
     }
 
     public void readMessageFromTopic(String topicName) {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", connection.getProps().get("bootstrapServers"));
-        props.put("group.id", getName());
-        props.put("auto.offset.reset", "earliest");
-        props.put("enable.auto.commit", "true");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        Properties props = new Properties();
+//        props.put("bootstrap.servers", connection.getProps().get("bootstrapServers"));
+//        props.put("group.id", getName());
+//        props.put("auto.offset.reset", "earliest");
+//        props.put("enable.auto.commit", "true");
+//        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         log.info("hahaha" + topicName);
 
     }
