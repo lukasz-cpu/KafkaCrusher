b148cb59eb35a713f0fe25c779222a63b991065f
lukasz_p <merc.angel@hotmail.com>
2021-10-12 17:39:15 +0200

    asd

---
+++ src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.kafkacrusher.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConnectionControllerTest {
+
+    @Test
+    void connect() {
+
+    }
+
+    @Test
+    void getConnections() {
+    }
+}
