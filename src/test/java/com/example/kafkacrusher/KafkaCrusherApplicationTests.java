5d8c50ec799e1cbd4c4aab0348dbce8c19d4da3a
lukasz_p <merc.angel@hotmail.com>
2021-10-12 16:52:13 +0200

    asd

--- src/test/java/com/example/kafkacrusher/KafkaCrusherApplicationTests.java
+++ src/test/java/com/example/kafkacrusher/KafkaCrusherApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.kafkacrusher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class KafkaCrusherApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1,2);
     }
 
 }
