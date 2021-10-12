e8b82978fae15dadc0264c26e1fd55b25d781fce
lukasz_p <merc.angel@hotmail.com>
2021-10-12 11:20:37 +0200

    234234

--- src/main/java/com/example/kafkacrusher/connection/RegisterClientException.java
+++
@@ -1,8 +0,0 @@
-package com.example.kafkacrusher.connection;
-
-public class RegisterClientException extends RuntimeException {
-    public RegisterClientException(String errorMessage) {
-        super(errorMessage);
-    }
-}
-
