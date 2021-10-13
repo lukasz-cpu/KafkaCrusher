374ced8309e25c95d9111bd3f1e4334a4ce108f8
lukasz_p <merc.angel@hotmail.com>
2021-10-13 21:03:15 +0200

    123

---
+++ src/test/java/com/example/kafkacrusher/topic/TopicControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.kafkacrusher.topic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopicControllerTest {
+
+    @Test
+    void getTopicsForConnectionName() {
+    }
+
+    @Test
+    void addTopicsForConnectionName() {
+    }
+
+    @Test
+    void deleteTopicsForConnectionName() {
+    }
+}
