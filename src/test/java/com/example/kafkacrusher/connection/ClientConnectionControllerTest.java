24cd9b7ce3e50b66514c1ce63c647f724696f5d2
lukasz_p <merc.angel@hotmail.com>
2021-10-13 13:53:30 +0200

    123

--- src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
+++ src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
@@ -4,7 +4,6 @@ import com.example.kafkacrusher.KafkaCrusherApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
@@ -14,14 +13,10 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.client.RestTemplate;
 
 import static com.example.kafkacrusher.util.JsonTestUtil.getJson;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(SpringExtension.class)
@@ -32,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(locations = "classpath:application-test.properties")
 @Slf4j
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://192.168.0.74:9092", "port=9092" })
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://192.168.0.74:9092", "port=9092"})
 class ClientConnectionControllerTest {
 
 
@@ -40,16 +35,9 @@ class ClientConnectionControllerTest {
     private final String url = "http://localhost:8099"; //defined in props
 
 
-
-
-
     @Test
     void connect() {
         //given
-
-
-
-
         ClientConnectionRequestDTO connection_test = ClientConnectionRequestDTO.builder()
                 .connectionName("connection test")
                 .brokers("192.168.0.74:9092")
@@ -57,14 +45,14 @@ class ClientConnectionControllerTest {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>(getJson(connection_test), headers);
+
+        //when
         String response = restTemplate.postForObject(url + "/registerConnection", entity, String.class);
 
-
+        //then
         assertTrue(response.contains("Connection added"));
         assertTrue(response.contains("\"connectionName\" : \"connection test\""));
         assertTrue(response.contains("\"brokers\" : \"192.168.0.74:9092\""));
-
-
     }
 
 } 
