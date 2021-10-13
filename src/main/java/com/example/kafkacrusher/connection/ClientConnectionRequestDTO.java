45269eb96c8f66206ed130ced7dd507c3d322991
lukasz_p <merc.angel@hotmail.com>
2021-10-13 09:11:02 +0200

    234123

--- src/main/java/com/example/kafkacrusher/connection/ClientConnectionRequestDTO.java
+++ src/main/java/com/example/kafkacrusher/connection/ClientConnectionRequestDTO.java
@@ -1,11 +1,13 @@
 package com.example.kafkacrusher.connection;
 
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
+@Builder
 public class ClientConnectionRequestDTO {
     private String connectionName;
     private String brokers;
--- src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
+++ src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
@@ -2,12 +2,19 @@ package com.example.kafkacrusher.connection;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class ClientConnectionControllerTest {
 
     @Test
     void connect() {
+        //given
+        ClientConnectionRequestDTO.builder()
+                .connectionName("connection test")
+                .brokers("192.168.0.74:9092");
+        //when
+        
 
     }
 
