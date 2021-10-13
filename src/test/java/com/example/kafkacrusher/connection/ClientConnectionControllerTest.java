122285a2ca7aee889a7353625351d1e1807d9958
lukasz_p <merc.angel@hotmail.com>
2021-10-13 10:37:33 +0200

    asd

--- src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
+++ src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
@@ -1,20 +1,24 @@
 package com.example.kafkacrusher.connection;
 
+import com.example.kafkacrusher.KafkaCrusherApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        classes = KafkaCrusherApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ClientConnectionControllerTest {
 
     @Test
-    void connect() {
-        //given
-        ClientConnectionRequestDTO.builder()
-                .connectionName("connection test")
-                .brokers("192.168.0.74:9092");
-        //when
-        
+    void connect() throws InterruptedException {
+
 
     }
 
