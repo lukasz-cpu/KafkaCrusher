431b529d72cb54fcafe042db8d0130ae46aedc4d
lukasz_p <merc.angel@hotmail.com>
2021-10-13 11:07:43 +0200

    ads

--- src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
+++ src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
@@ -1,12 +1,18 @@
 package com.example.kafkacrusher.connection;
 
 import com.example.kafkacrusher.KafkaCrusherApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
 
 
 @ExtendWith(SpringExtension.class)
@@ -15,12 +21,25 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
 )
 @TestPropertySource(locations = "classpath:application-test.properties")
+@Slf4j
 class ClientConnectionControllerTest {
 
+    private final RestTemplate restTemplate = new TestRestTemplate().getRestTemplate();
+    private final String url = "http://localhost:8099"; //defined in props
 
     @Test
     void connect() {
-        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        String requestJson = "";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(requestJson,headers);
+        String answer = restTemplate.postForObject(url+"/registerConnection", entity, String.class);
+
+        log.info(answer);
+
+
 
 
     }
