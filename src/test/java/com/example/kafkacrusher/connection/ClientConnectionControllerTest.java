265e908c9e90b6c88573aee701ec0d168e1ba832
lukasz_p <merc.angel@hotmail.com>
2021-10-13 10:44:19 +0200

    asd

--- src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
+++ src/test/java/com/example/kafkacrusher/connection/ClientConnectionControllerTest.java
@@ -4,6 +4,7 @@ import com.example.kafkacrusher.KafkaCrusherApplication;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,8 +17,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ClientConnectionControllerTest {
 
+
     @Test
-    void connect() throws InterruptedException {
+    void connect() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
 
 
     }
