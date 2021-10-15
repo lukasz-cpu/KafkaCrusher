6eab02ee129d3554355b43a155d2fd1b3c67608d
lukasz_p <merc.angel@hotmail.com>
2021-10-15 14:38:21 +0200

    ads

--- src/test/java/com/example/kafkacrusher/messages/ReadMessagesFromTopicTests.java
+++ src/test/java/com/example/kafkacrusher/messages/ReadMessagesFromTopicTests.java
@@ -1,81 +1,81 @@
-package com.example.kafkacrusher.messages;
-
-import com.example.kafkacrusher.KafkaCrusherApplication;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Properties;
-
-import static com.example.kafkacrusher.util.JsonTestUtil.getJson;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(
-        classes = KafkaCrusherApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
-)
-@TestPropertySource(locations = "classpath:application-test.properties")
-@Slf4j
-@DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"}, topics = {"TestTopic"})
-class ReadMessagesFromTopicTests {
-
-
-    public static KafkaProducer<String, String> kafkaProducer = null;
-    private final RestTemplate restTemplate = new TestRestTemplate().getRestTemplate();
-
-    @BeforeEach
-    void setUp() {
-        send("TestTopic", "test message on test topic");
-    }
-
-    @Test
-    void readMessagesFromTopic() {
-        final String baseUrl = "localhost:8099/readMessagesFromTopic?connectionName=connection test10&topicName=TestTopic";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<MessageRequestDTO> response = restTemplate.exchange(baseUrl, HttpMethod.GET, entity,MessageRequestDTO.class);
-
-        log.info("RESPONSE------------");
-        log.info(response.getBody().toString());
-        log.info("----------------------------");
-        System.out.println(response.getBody().toString());
-    }
-
-
-
-
-    public static KafkaProducer<String, String> createKafkaProducer() {
-        Properties properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        KafkaProducer<String, String> producer = null;
-        try {
-            producer = new KafkaProducer<>(properties);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return producer;
-    }
-
-    public static void send(String topic, String msg) {
-        if (kafkaProducer == null) {
-            kafkaProducer = createKafkaProducer();
-        }
-        kafkaProducer.send(new ProducerRecord<>(topic, msg));
-    }
-}
+//package com.example.kafkacrusher.messages;
+//
+//import com.example.kafkacrusher.KafkaCrusherApplication;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.ProducerRecord;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.http.*;
+//import org.springframework.kafka.test.context.EmbeddedKafka;
+//import org.springframework.test.annotation.DirtiesContext;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.Properties;
+//
+//import static com.example.kafkacrusher.util.JsonTestUtil.getJson;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest(
+//        classes = KafkaCrusherApplication.class,
+//        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+//)
+//@TestPropertySource(locations = "classpath:application-test.properties")
+//@Slf4j
+//@DirtiesContext
+//@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"}, topics = {"TestTopic"})
+//class ReadMessagesFromTopicTests {
+//
+//
+//    public static KafkaProducer<String, String> kafkaProducer = null;
+//    private final RestTemplate restTemplate = new TestRestTemplate().getRestTemplate();
+//
+//    @BeforeEach
+//    void setUp() {
+//        send("TestTopic", "test message on test topic");
+//    }
+//
+//    @Test
+//    void readMessagesFromTopic() {
+//        final String baseUrl = "localhost:8099/readMessagesFromTopic?connectionName=connection test10&topicName=TestTopic";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        HttpEntity<String> entity = new HttpEntity<>(headers);
+//
+//        ResponseEntity<MessageRequestDTO> response = restTemplate.exchange(baseUrl, HttpMethod.GET, entity,MessageRequestDTO.class);
+//
+//        log.info("RESPONSE------------");
+//        log.info(response.getBody().toString());
+//        log.info("----------------------------");
+//        System.out.println(response.getBody().toString());
+//    }
+//
+//
+//
+//
+//    public static KafkaProducer<String, String> createKafkaProducer() {
+//        Properties properties = new Properties();
+//        properties.put("bootstrap.servers", "localhost:9092");
+//        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        KafkaProducer<String, String> producer = null;
+//        try {
+//            producer = new KafkaProducer<>(properties);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return producer;
+//    }
+//
+//    public static void send(String topic, String msg) {
+//        if (kafkaProducer == null) {
+//            kafkaProducer = createKafkaProducer();
+//        }
+//        kafkaProducer.send(new ProducerRecord<>(topic, msg));
+//    }
+//}
