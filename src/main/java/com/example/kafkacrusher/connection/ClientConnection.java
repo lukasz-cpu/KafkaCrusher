1a255d8b0f3e7ed3b2dd4800e7a316264c4425b6
lukasz_p <merc.angel@hotmail.com>
2021-10-15 15:36:13 +0200

    234

--- src/main/java/com/example/kafkacrusher/connection/ClientConnection.java
+++ src/main/java/com/example/kafkacrusher/connection/ClientConnection.java
@@ -10,8 +10,9 @@ import javax.persistence.*;
 @Entity(name = "client_connection")
 public class ClientConnection {
 
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   //FIX ME need to investigate UUID as ID to have better hash code equals
     private Long id;
     @Column(unique = true)
     private String connectionName;
