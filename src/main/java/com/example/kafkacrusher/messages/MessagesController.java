167b235f6f6b551bea06a67a0355eef5599c74fc
lukasz_p <merc.angel@hotmail.com>
2021-10-20 20:49:24 +0200

    asd

--- src/main/java/com/example/kafkacrusher/messages/MessagesController.java
+++ src/main/java/com/example/kafkacrusher/messages/MessagesController.java
@@ -24,7 +24,7 @@ public class MessagesController {
         try {
             MessageRequestDTO messageRequestDTO = messageService.processMessage(message);
             return new ResponseEntity<>(messageRequestDTO, HttpStatus.OK);
-        } catch (MessageProcessingException | BrokerNotFoundException | TopicsNameNotFound e) {
+        } catch (MessageProcessingException | TopicsNameNotFound e) {
             return new ResponseEntity<>(message, HttpStatus.CONFLICT);
         }
     }
