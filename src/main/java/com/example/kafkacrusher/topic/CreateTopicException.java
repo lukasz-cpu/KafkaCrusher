61cd60f31005b584436c9e5b7f8f7f6cdcdefbba
lukasz_p <merc.angel@hotmail.com>
2021-10-20 17:18:18 +0200

    asd

--- src/main/java/com/example/kafkacrusher/topic/CreateTopicException.java
+++
@@ -1,6 +0,0 @@
-package com.example.kafkacrusher.topic;
-
-public class CreateTopicException extends Exception {
-    public CreateTopicException(String s) {
-    }
-}
--- src/main/java/com/example/kafkacrusher/topic/DeleteTopicException.java
+++
@@ -1,6 +0,0 @@
-package com.example.kafkacrusher.topic;
-
-public class DeleteTopicException extends Exception {
-    public DeleteTopicException(String s) {
-    }
-}
