1f6c119f365679ea5cdd9150359c54b227f81b4a
lukasz_p <merc.angel@hotmail.com>
2021-12-21 22:23:10 +0100

    asd

--- src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
+++ src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
@@ -14,3 +14,6 @@ public class KafkaCrusherApplication {
 //ZkUtils.getBrokerInfo( 
 //https://github.com/metamx/incubator-storm/blob/f6b843d99c874a7139c7339b79cfc6a995f143d2/external/storm-kafka/src/jvm/storm/kafka/DynamicBrokersReader.java#L35
 // zaczac przechowywac jako mapa??
+
+
+//connection name1 ->> broker1 --> mapa<adres, active>
