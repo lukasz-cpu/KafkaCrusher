6f08d4cb3c2a18ce768f8e764b0804c3340dab70
lukasz_p <merc.angel@hotmail.com>
2021-12-22 19:08:07 +0100

    123

---
+++ src/main/java/com/example/kafkacrusher/connection/ClientConnection2.java
@@ -0,0 +1,29 @@
+package com.example.kafkacrusher.connection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity(name = "client_connection2")
+public class ClientConnection2 {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //FIX ME need to investigate UUID as ID to have better hash code equals
+    private Long id;
+    @Column(unique = true)
+    private String connectionName;
+    private String brokers;
+    @Column(nullable = false)
+    private Boolean isActive;
+
+
+}
--- src/main/java/com/example/kafkacrusher/connection/model/Broker.java
+++ src/main/java/com/example/kafkacrusher/connection/model/Broker.java
@@ -3,9 +3,11 @@ package com.example.kafkacrusher.connection.model;
 
 import lombok.Data;
 
+import javax.persistence.Embeddable;
 import java.util.Map;
 
 @Data
+@Embeddable
 public class Broker {
     private Map<Address, ActiveStatus> serverAddresses;
 }
