1e46f1ab5ac409455bcd0a32a88f5b76d7b6bd1e
lukasz_p <merc.angel@hotmail.com>
2021-12-23 10:41:28 +0100

    asd

--- src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
+++ src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
@@ -1,26 +1,13 @@
 package com.example.kafkacrusher;
 
-import com.example.kafkacrusher.connection.ClientConnection2;
-import com.example.kafkacrusher.connection.ClientConnectionRepository;
-import com.example.kafkacrusher.connection.ClientConnectionRepository2;
-import com.example.kafkacrusher.connection.model.ActiveStatus;
-import com.example.kafkacrusher.connection.model.Address;
-import com.example.kafkacrusher.connection.model.Broker;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @SpringBootApplication
 public class KafkaCrusherApplication implements CommandLineRunner {
 
-
-    @Autowired
-    ClientConnectionRepository2 clientConnectionRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaCrusherApplication.class, args);
@@ -28,39 +15,6 @@ public class KafkaCrusherApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-
-        Address address = new Address("1231");
-        ActiveStatus active = new ActiveStatus("true");
-
-
-        Map<Address, ActiveStatus> resultMap = new HashMap<>();
-
-        resultMap.put(address, active);
-
-
-        Broker broker = new Broker(resultMap);
-
-
-        ClientConnection2 clientConnection2 = ClientConnection2.ClientConnection2Builder.aClientConnection2()
-                .withConnectionName("LAMBO33")
-                .withBrokers(broker)
-                .build();
-
-
-        ClientConnection2 save = clientConnectionRepository.save(clientConnection2);
-
-
-        System.out.println("-----------------------------------------------");
-
-
-        List<ClientConnection2> all = clientConnectionRepository.findAll();
-
-
-        for (ClientConnection2 connection2 : all) {
-            System.out.println(connection2.toString());
-        }
-
 
     }
 }
