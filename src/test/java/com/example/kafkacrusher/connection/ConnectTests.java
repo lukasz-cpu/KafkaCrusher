0795cb20744fe3af9e9c9dd25f625880ba457b2a
lukasz_p <merc.angel@hotmail.com>
2022-06-09 15:58:52 +0200

    fix

--- src/test/java/com/example/kafkacrusher/connection/ConnectTests.java
+++ src/test/java/com/example/kafkacrusher/connection/ConnectTests.java
@@ -4,9 +4,15 @@ import com.example.kafkacrusher.KafkaCrusherApplication;
 import com.example.kafkacrusher.connection.dto.ClientConnectionDTO;
 import com.example.kafkacrusher.util.GsonTestUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.protocol.types.Field;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.BasicJsonTester;
+import org.springframework.boot.test.json.JsonContent;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +22,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
+
 
 
 @ExtendWith(SpringExtension.class)
@@ -30,7 +37,7 @@ class ConnectTests {
     private final RestTemplate restTemplate = new TestRestTemplate().getRestTemplate();
 
     @Test
-    void connect() {
+    void connect() throws JSONException {
         //given
         ClientConnectionDTO connection_test = ClientConnectionDTO.builder().connectionName("connection test").brokerAddress("localhost:9092").build();
         HttpHeaders headers = new HttpHeaders();
@@ -44,9 +51,24 @@ class ConnectTests {
         String response = restTemplate.postForObject(url + "/registerConnection", entity, String.class);
 
 
-        System.out.println(response);
+        var expectedResult = "{\n" +
+                "  \"connectionName\": \"connection test\",\n" +
+                "  \"broker\": {\n" +
+                "    \"serverAddresses\": [\n" +
+                "      [\n" +
+                "        {\n" +
+                "          \"address\": \"localhost:9092\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"isActive\": false\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    ]\n" +
+                "  }\n" +
+                "}";
 
-
+        JSONAssert.assertEquals(
+                expectedResult, response, JSONCompareMode.LENIENT);
     }
 }
 
