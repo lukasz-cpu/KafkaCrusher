8bdadff8f2a5da83246fda7f6be5c6c3367ac214
lukasz_p <merc.angel@hotmail.com>
2022-06-09 10:05:00 +0200

    fix

--- src/main/java/com/example/kafkacrusher/connection/entity/ActiveStatus.java
+++ src/main/java/com/example/kafkacrusher/connection/entity/ActiveStatus.java
@@ -17,6 +17,6 @@ import javax.persistence.Embeddable;
 @ToString
 @Embeddable
 @Builder
-public class ActiveStatus {  //ENUM TODO
-   public String activeStatus;
+public class ActiveStatus { 
+   public boolean isActive;
 }
