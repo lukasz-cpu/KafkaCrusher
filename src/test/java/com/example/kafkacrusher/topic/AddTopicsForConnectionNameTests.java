9fa5a9ad74fae41535de0c1dff481d9984385064
lukasz_p <merc.angel@hotmail.com>
2022-06-13 18:40:03 +0200

    fix

--- src/test/java/com/example/kafkacrusher/topic/AddTopicsForConnectionNameTests.java
+++ src/test/java/com/example/kafkacrusher/topic/AddTopicsForConnectionNameTests.java
@@ -1,13 +1,20 @@
 package com.example.kafkacrusher.topic;
 
 import com.example.kafkacrusher.KafkaCrusherApplication;
+import com.example.kafkacrusher.connection.ClientConnectionRepository;
+import com.example.kafkacrusher.connection.entity.ActiveStatus;
+import com.example.kafkacrusher.connection.entity.Address;
+import com.example.kafkacrusher.connection.entity.Broker;
+import com.example.kafkacrusher.connection.entity.ClientConnection;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
@@ -21,7 +28,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.example.kafkacrusher.util.JsonTestUtil.getJson;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -39,13 +48,47 @@ class AddTopicsForConnectionNameTests {
 
     private final RestTemplate restTemplate = new TestRestTemplate().getRestTemplate();
     private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ClientConnectionRepository clientConnectionRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        clientConnectionRepository.deleteAll();
+
+        Map<Address, ActiveStatus> resultMap = new HashMap<>();
+
+
+        Address build = Address.builder()
+                .address("localhost:9092")
+                .build();
+
+        ActiveStatus trueOne = ActiveStatus
+                .builder()
+                .isActive(true)
+                .build();
+
+
+
+        resultMap.put(build, trueOne);
+
+        Broker result = Broker.builder().serverAddresses(resultMap).build();
+
+        ClientConnection connection_one1 = ClientConnection.ClientConnectionBuilder.aClientConnection()
+                .withConnectionName("connection_one13434")
+                .withBrokers(result)
+                .build();
+
+        clientConnectionRepository.save(connection_one1);
+
+    }
 
     @Test
     void addTopicsForConnectionName() throws JsonProcessingException {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        final String baseUrl = "http://localhost:8099/addTopicsForConnectionName?connectionName=connection test10";
+        final String baseUrl = "http://localhost:8099/addTopicsForConnectionName?connectionName=connection_one13434";
         TopicListDTO request = TopicListDTO.builder().topicList(Arrays.asList("TestTopic2", "TestTopic3", "TestTopic4", "Topic7")).build();
 
 
@@ -70,4 +113,6 @@ class AddTopicsForConnectionNameTests {
         );
 
     }
+
+
 }
