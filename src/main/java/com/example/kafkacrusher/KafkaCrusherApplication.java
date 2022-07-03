ed415bcfd43b69b4ae010c59daa6d83204ccc15a
lukasz_p <merc.angel@hotmail.com>
2022-07-03 18:30:40 +0200

    f

--- src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
+++ src/main/java/com/example/kafkacrusher/KafkaCrusherApplication.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashMap;
 import java.util.List;
